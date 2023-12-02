6fc252eb886551b58fedbd77f39bc0c023821675
Usatiy Nyan <kirillarionov123456@gmail.com>
2023-12-02 16:10:25 +0100

    stb_image dependency

---
+++ dependencies/stb/include/stb/image.hpp
@@ -0,0 +1,22 @@
+//
+// Created by usatiynyan on 12/2/23.
+//
+
+#pragma once
+
+#include <filesystem>
+#include <memory>
+#include <tl/optional.hpp>
+
+namespace stb {
+struct Image {
+    using Deleter = void (*)(uint8_t *);
+
+    std::unique_ptr<uint8_t, Deleter> data;
+    int width;
+    int height;
+    int channels;
+};
+
+tl::optional<Image> image_load(const std::filesystem::path& path, int desired_channels = 0, bool flip_vertically = true);
+} // namespace stb
---
+++ dependencies/stb/src/image.cpp
@@ -0,0 +1,33 @@
+//
+// Created by usatiynyan on 12/2/23.
+//
+
+#include "stb/image.hpp"
+
+#include <assert.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ASSERT(x) ASSERT(x)
+
+#include "stb_image.h"
+
+#undef STBI_ASSERT
+#undef STB_IMAGE_IMPLEMENTATION
+
+namespace stb {
+tl::optional<Image> image_load(const std::filesystem::path& path, int desired_channels, bool flip_vertically) {
+    stbi_set_flip_vertically_on_load(flip_vertically);
+
+    int width = 0;
+    int height = 0;
+    int channels = 0;
+    std::unique_ptr<uint8_t, Image::Deleter> data{
+        stbi_load(path.c_str(), &width, &height, &channels, desired_channels),
+        [](uint8_t* image_data) { stbi_image_free(static_cast<void*>(image_data)); },
+    };
+    if (!data) {
+        return tl::nullopt;
+    }
+    return Image{ std::move(data), width, height, channels };
+}
+} // namespace stb
