bf76a33a27b8893dd19cee17ca6fdece3181b270
Usatiy Nyan <kirillarionov123456@gmail.com>
2023-12-21 23:54:07 +0100

    cmake dependencies refactoring

---
+++ dependencies/boost.cmake
@@ -0,0 +1,52 @@
+
+# ew boost, oh well...
+cpmaddpackage(
+        NAME boost_config
+        GIT_REPOSITORY "git@github.com:boostorg/config.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_mp11
+        GIT_REPOSITORY "git@github.com:boostorg/mp11.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_describe
+        GIT_REPOSITORY "git@github.com:boostorg/describe.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_type_traits
+        GIT_REPOSITORY "git@github.com:boostorg/type_traits.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_container_hash
+        GIT_REPOSITORY "git@github.com:boostorg/container_hash.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_intrusive
+        GIT_REPOSITORY "git@github.com:boostorg/intrusive.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_move
+        GIT_REPOSITORY "git@github.com:boostorg/move.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_assert
+        GIT_REPOSITORY "git@github.com:boostorg/assert.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_static_assert
+        GIT_REPOSITORY "git@github.com:boostorg/static_assert.git"
+        GIT_TAG "boost-1.83.0"
+)
+cpmaddpackage(
+        NAME boost_container
+        GIT_REPOSITORY "git@github.com:boostorg/container.git"
+        GIT_TAG "boost-1.83.0"
+)
