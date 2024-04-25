33980f668e37ca0f60c4edc0fcb961e3c86f6118
UsatiyNyan <us4tiyny4n@gmail.com>
2024-04-25 22:11:30 +0200

    rework texture, simplify

--- include/sl/gfx/vtx/texture.hpp
+++ include/sl/gfx/vtx/texture.hpp
@@ -4,71 +4,73 @@
 
 #pragma once
 
-#include "sl/meta/lifetime/finalizer.hpp"
 #include "sl/gfx/common/log.hpp"
 #include "sl/gfx/common/vendors.hpp"
-#include "sl/gfx/primitives/type_map.hpp"
+#include "sl/gfx/primitives/gl_type_map.hpp"
+
+#include <sl/meta/lifetime/finalizer.hpp>
+#include <sl/meta/lifetime/unique.hpp>
 
 #include <span>
 #include <tl/optional.hpp>
 
 namespace sl::gfx {
 
-enum class TextureType : GLenum {
-    TEXTURE_1D = GL_TEXTURE_1D,
-    TEXTURE_2D = GL_TEXTURE_2D,
-    TEXTURE_3D = GL_TEXTURE_3D,
-    TEXTURE_1D_ARRAY = GL_TEXTURE_1D_ARRAY,
-    TEXTURE_2D_ARRAY = GL_TEXTURE_2D_ARRAY,
-    TEXTURE_RECTANGLE = GL_TEXTURE_RECTANGLE,
-    TEXTURE_CUBE_MAP = GL_TEXTURE_CUBE_MAP,
-    TEXTURE_CUBE_MAP_ARRAY = GL_TEXTURE_CUBE_MAP_ARRAY,
-    TEXTURE_BUFFER = GL_TEXTURE_BUFFER,
-    TEXTURE_2D_MULTISAMPLE = GL_TEXTURE_2D_MULTISAMPLE,
-    TEXTURE_2D_MULTISAMPLE_ARRAY = GL_TEXTURE_2D_MULTISAMPLE_ARRAY,
+enum class texture_type : GLenum {
+    texture_1d = GL_TEXTURE_1D,
+    texture_2d = GL_TEXTURE_2D,
+    texture_3d = GL_TEXTURE_3D,
+    texture_1d_array = GL_TEXTURE_1D_ARRAY,
+    texture_2d_array = GL_TEXTURE_2D_ARRAY,
+    texture_rectangle = GL_TEXTURE_RECTANGLE,
+    texture_cube_map = GL_TEXTURE_CUBE_MAP,
+    texture_cube_map_array = GL_TEXTURE_CUBE_MAP_ARRAY,
+    texture_buffer = GL_TEXTURE_BUFFER,
+    texture_2d_multisample = GL_TEXTURE_2D_MULTISAMPLE,
+    texture_2d_multisample_array = GL_TEXTURE_2D_MULTISAMPLE_ARRAY,
 };
 
-enum class TextureWrap : GLint {
-    REPEAT = GL_REPEAT,
-    MIRRORED_REPEAT = GL_MIRRORED_REPEAT,
-    CLAMP_TO_EDGE = GL_CLAMP_TO_EDGE,
-    CLAMP_TO_BORDER = GL_CLAMP_TO_BORDER,
-    MIRROR_CLAMP_TO_EDGE = GL_MIRROR_CLAMP_TO_EDGE,
+enum class texture_wrap : GLint {
+    repeat = GL_REPEAT,
+    mirrored_repeat = GL_MIRRORED_REPEAT,
+    clamp_to_edge = GL_CLAMP_TO_EDGE,
+    clamp_to_border = GL_CLAMP_TO_BORDER,
+    mirror_clamp_to_edge = GL_MIRROR_CLAMP_TO_EDGE,
 };
 
-enum class TextureFilter : GLint {
-    NEAREST = GL_NEAREST,
-    LINEAR = GL_LINEAR,
-    NEAREST_MIPMAP_NEAREST = GL_NEAREST_MIPMAP_NEAREST,
-    LINEAR_MIPMAP_NEAREST = GL_LINEAR_MIPMAP_NEAREST,
-    NEAREST_MIPMAP_LINEAR = GL_NEAREST_MIPMAP_LINEAR,
-    LINEAR_MIPMAP_LINEAR = GL_LINEAR_MIPMAP_LINEAR,
+enum class texture_filter : GLint {
+    nearest = GL_NEAREST,
+    linear = GL_LINEAR,
+    nearest_mipmap_nearest = GL_NEAREST_MIPMAP_NEAREST,
+    linear_mipmap_nearest = GL_LINEAR_MIPMAP_NEAREST,
+    nearest_mipmap_linear = GL_NEAREST_MIPMAP_LINEAR,
+    linear_mipmap_linear = GL_LINEAR_MIPMAP_LINEAR,
 };
 
-struct TextureFormat {
+struct texture_format {
     // TODO(types): enum types and size mapping
-    explicit TextureFormat(GLenum internal_and_input)
+    explicit texture_format(GLenum internal_and_input)
         : internal{ static_cast<GLint>(internal_and_input) }, input{ internal_and_input } {}
 
-    TextureFormat(GLint an_internal, GLenum an_input) : internal{ an_internal }, input{ an_input } {}
+    texture_format(GLint an_internal, GLenum an_input) : internal{ an_internal }, input{ an_input } {}
 
 public:
     GLint internal;
     GLenum input;
 };
 
-template <TextureType type_>
-class Texture : public meta::finalizer<Texture<type_>> {
-    template <TextureType>
-    friend class TextureBuilder;
+template <texture_type type>
+class bound_texture;
 
-    Texture()
-        : meta::finalizer<Texture>{ [](Texture& self) {
+template <texture_type type>
+class texture : public meta::finalizer<texture<type>> {
+    texture()
+        : meta::finalizer<texture>{ [](texture& self) {
               // TODO(@usatiynyan): more than one texture?
-              glDeleteTextures(1, &self.object_);
-              LOG_DEBUG("glDeleteTextures: {}", self.object_);
+              glDeleteTextures(1, &self.internal_);
+              LOG_DEBUG("glDeleteTextures: {}", self.internal_);
           } },
-          object_{ [] {
+          internal_{ [] {
               GLuint texture;
               glGenTextures(1, &texture);
               LOG_DEBUG("glGenTextures: {}", texture);
@@ -76,91 +78,101 @@ class Texture : public meta::finalizer<Texture<type_>> {
           }() } {}
 
 public:
-    class ConstBind : public meta::finalizer<ConstBind> {
-    public:
-        explicit ConstBind(const Texture& tex) : meta::finalizer<ConstBind> {
-            [](ConstBind&) {
-                // TODO(@usatiynyan): can be omitted in "optimized" build
-                glBindTexture(static_cast<GLenum>(type_), 0);
-                LOG_DEBUG("glBindTexture({}): 0", static_cast<GLenum>(type_));
-            }
-        }
-        {
-            glBindTexture(static_cast<GLenum>(type_), *tex);
-            LOG_DEBUG("glBindTexture({}): {}", static_cast<GLenum>(type_), *tex);
-        }
-    };
+    [[nodiscard]] bound_texture<type> bind();
+    [[nodiscard]] const bound_texture<type> bind() const;
+    template <std::size_t unit>
+    [[nodiscard]] const bound_texture<type> activate() const;
 
-    class Bind : private ConstBind {
-    public:
-        explicit Bind(Texture& tex) : ConstBind{ tex } {}
-
-        void set_parameter(GLenum key, GLint value) { glTexParameteri(static_cast<GLenum>(type_), key, value); }
-    };
-
-public:
-    [[nodiscard]] GLuint operator*() const { return object_; }
-    [[nodiscard]] auto bind() const { return ConstBind{ *this }; }
-    [[nodiscard]] auto bind() { return Bind{ *this }; }
-    template <std::size_t unit_>
-    [[nodiscard]] auto activate() const {
-        // TODO(@usatiynyan): static_assert(index < GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS);
-        static_assert(unit_ < 16, "texture_units can be < 16, if this is incorrect check TODO above");
-        glActiveTexture(GL_TEXTURE0 + static_cast<GLenum>(unit_));
-        return bind();
-    }
+    [[nodiscard]] GLuint internal() const { return internal_; }
 
 private:
-    GLuint object_;
+    GLuint internal_;
 };
 
-template <TextureType type_>
-class TextureBuilder {
+template <texture_type type>
+class bound_texture
+    : public
+#ifdef NDEBUG
+      meta::unique
+#else
+      meta::finalizer<bound_texture<type>>
+#endif
+{
 public:
-    void set_wrap_s(TextureWrap wrap) { bind_->set_parameter(GL_TEXTURE_WRAP_S, static_cast<GLint>(wrap)); }
-    void set_wrap_t(TextureWrap wrap) { bind_->set_parameter(GL_TEXTURE_WRAP_T, static_cast<GLint>(wrap)); }
-    void set_wrap_r(TextureWrap wrap) { bind_->set_parameter(GL_TEXTURE_WRAP_R, static_cast<GLint>(wrap)); }
-
-    void set_min_filter(TextureFilter filter) {
-        bind_->set_parameter(GL_TEXTURE_MIN_FILTER, static_cast<GLint>(filter));
+    explicit bound_texture(const texture<type>& texture)
+#ifndef NDEBUG
+        : meta::finalizer<bound_texture>{ [](bound_texture&) {
+              glBindTexture(static_cast<GLenum>(type), 0);
+              LOG_DEBUG("glBindTexture({}): 0", static_cast<GLenum>(type));
+          } }
+#endif
+    {
+        glBindTexture(static_cast<GLenum>(type), texture.internal());
+        LOG_DEBUG("glBindTexture({}): {}", static_cast<GLenum>(type), texture.internal());
     }
-    void set_max_filter(TextureFilter filter) {
-        bind_->set_parameter(GL_TEXTURE_MAG_FILTER, static_cast<GLint>(filter));
+
+    void set_parameter(GLenum key, GLint value) { glTexParameteri(static_cast<GLenum>(type), key, value); }
+};
+
+template <texture_type type>
+bound_texture<type> texture<type>::bind() {
+    return bound_texture{ *this };
+}
+
+template <texture_type type>
+const bound_texture<type> texture<type>::bind() const {
+    return bound_texture{ *this };
+}
+
+template <texture_type type>
+template <std::size_t unit>
+const bound_texture<type> texture<type>::activate() const {
+    // TODO(@usatiynyan): static_assert(index < GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS);
+    static_assert(unit < 16, "texture_units can be < 16, if this is incorrect check TODO above");
+    glActiveTexture(GL_TEXTURE0 + static_cast<GLenum>(unit));
+    return bind();
+}
+
+template <texture_type type>
+class texture_builder : public meta::unique {
+public:
+    void set_wrap_s(texture_wrap wrap) { bound_->set_parameter(GL_TEXTURE_WRAP_S, static_cast<GLint>(wrap)); }
+    void set_wrap_t(texture_wrap wrap) { bound_->set_parameter(GL_TEXTURE_WRAP_T, static_cast<GLint>(wrap)); }
+    void set_wrap_r(texture_wrap wrap) { bound_->set_parameter(GL_TEXTURE_WRAP_R, static_cast<GLint>(wrap)); }
+
+    void set_min_filter(texture_filter filter) {
+        bound_->set_parameter(GL_TEXTURE_MIN_FILTER, static_cast<GLint>(filter));
+    }
+    void set_max_filter(texture_filter filter) {
+        bound_->set_parameter(GL_TEXTURE_MAG_FILTER, static_cast<GLint>(filter));
     }
 
-    template <std::size_t dimensions_count, typename DataType>
+    template <std::size_t dimensions_extent, typename T>
     void set_image(
-        const std::array<GLsizei, dimensions_count>& dimensions,
-        TextureFormat format,
-        const DataType* data,
+        std::span<const GLsizei, dimensions_extent> dimensions,
+        texture_format format,
+        const T* data,
         GLint lod = 0
     ) {
-        if constexpr (dimensions_count == 1ul) {
+        if constexpr (dimensions_extent == 1ul) {
             glTexImage1D(
-                static_cast<GLenum>(type_),
-                lod,
-                format.internal,
-                dimensions[0],
-                0,
-                format.input,
-                type_map<DataType>::value,
-                data
+                static_cast<GLenum>(type), lod, format.internal, dimensions[0], 0, format.input, gl_type_query<T>, data
             );
-        } else if constexpr (dimensions_count == 2ul) {
+        } else if constexpr (dimensions_extent == 2ul) {
             glTexImage2D(
-                static_cast<GLenum>(type_),
+                static_cast<GLenum>(type),
                 lod,
                 format.internal,
                 dimensions[0],
                 dimensions[1],
                 0,
                 format.input,
-                type_map<DataType>::value,
+                gl_type_query<T>,
                 data
             );
-        } else if constexpr (dimensions_count == 3ul) {
+        } else if constexpr (dimensions_extent == 3ul) {
             glTexImage3D(
-                static_cast<GLenum>(type_),
+                static_cast<GLenum>(type),
                 lod,
                 format.internal,
                 dimensions[0],
@@ -168,25 +180,25 @@ public:
                 dimensions[2],
                 0,
                 format.input,
-                type_map<DataType>::value,
+                gl_type_query<T>,
                 data
             );
         } else {
-            static_assert(dimensions_count <= 3ul, "only up to 3 dimensions");
+            static_assert(dimensions_extent <= 3ul, "only up to 3 dimensions");
         }
     }
 
-    Texture<type_> submit(bool generate_mipmap = true) && {
+    texture<type> submit(bool generate_mipmap = true) && {
         if (generate_mipmap) {
-            glGenerateMipmap(static_cast<GLenum>(type_));
+            glGenerateMipmap(static_cast<GLenum>(type));
         }
-        bind_.reset();
+        bound_.reset();
         return std::move(tex_).value();
     }
 
 private:
-    tl::optional<Texture<type_>> tex_{ Texture<type_>{} };
-    tl::optional<typename Texture<type_>::Bind> bind_{ tl::in_place, *tex_ };
+    tl::optional<texture<type>> tex_{ texture<type>{} };
+    tl::optional<bound_texture<type>> bound_{ tl::in_place, *tex_ };
 };
 
 } // namespace sl::gfx
