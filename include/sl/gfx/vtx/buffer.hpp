ac78e705e71ca13feb2e4df567cdc926f3dfd38d
UsatiyNyan <us4tiyny4n@gmail.com>
2024-04-25 20:33:20 +0200

    rework buffer, simplify

--- include/sl/gfx/vtx/buffer.hpp
+++ include/sl/gfx/vtx/buffer.hpp
@@ -9,143 +9,170 @@
 
 #include "sl/meta/lifetime/finalizer.hpp"
 
-#include <libassert/assert.hpp>
 #include <span>
+#include <tl/optional.hpp>
 
 namespace sl::gfx {
-enum class BufferType : GLenum {
-    ARRAY = GL_ARRAY_BUFFER, //  attributes
-    ATOMIC_COUNTER = GL_ATOMIC_COUNTER_BUFFER, // Atomic counter storage
-    COPY_READ = GL_COPY_READ_BUFFER, // Buffer copy source
-    COPY_WRITE = GL_COPY_WRITE_BUFFER, // Buffer copy destination
-    DISPATCH_INDIRECT = GL_DISPATCH_INDIRECT_BUFFER, // Indirect compute dispatch commands
-    DRAW_INDIRECT = GL_DRAW_INDIRECT_BUFFER, // Indirect command arguments
-    ELEMENT_ARRAY = GL_ELEMENT_ARRAY_BUFFER, //  array indices
-    PIXEL_PACK = GL_PIXEL_PACK_BUFFER, // Pixel read target
-    PIXEL_UNPACK = GL_PIXEL_UNPACK_BUFFER, // Texture data source
-    QUERY = GL_QUERY_BUFFER, // Query result buffer
-    SHADER_STORAGE = GL_SHADER_STORAGE_BUFFER, // Read-write storage for shaders
-    TEXTURE = GL_TEXTURE_BUFFER, // Texture data buffer
-    TRANSFORM_FEEDBACK = GL_TRANSFORM_FEEDBACK_BUFFER, // Transform feedback buffer
-    UNIFORM = GL_UNIFORM_BUFFER, // Uniform block storage
+enum class buffer_type : GLenum {
+    array = GL_ARRAY_BUFFER, // Vertex attributes
+    atomic_counter = GL_ATOMIC_COUNTER_BUFFER, // Atomic counter storage
+    copy_read = GL_COPY_READ_BUFFER, // Buffer copy source
+    copy_write = GL_COPY_WRITE_BUFFER, // Buffer copy destination
+    dispatch_indirect = GL_DISPATCH_INDIRECT_BUFFER, // Indirect compute dispatch commands
+    draw_indirect = GL_DRAW_INDIRECT_BUFFER, // Indirect command arguments
+    element_array = GL_ELEMENT_ARRAY_BUFFER, //  array indices
+    pixel_pack = GL_PIXEL_PACK_BUFFER, // Pixel read target
+    pixel_unpack = GL_PIXEL_UNPACK_BUFFER, // Texture data source
+    query = GL_QUERY_BUFFER, // Query result buffer
+    shader_storage = GL_SHADER_STORAGE_BUFFER, // Read-write storage for shaders
+    texture = GL_TEXTURE_BUFFER, // Texture data buffer
+    transform_feedback = GL_TRANSFORM_FEEDBACK_BUFFER, // Transform feedback buffer
+    uniform = GL_UNIFORM_BUFFER, // Uniform block storage
 };
 
-// STREAM - The data store contents will be modified once and used at most a few times.
-// STATIC - The data store contents will be modified once and used many times.
-// DYNAMIC - The data store contents will be modified repeatedly and used many times.
-// DRAW - The data store contents are modified by the application,
+// stream - The data store contents will be modified once and used at most a few times.
+// static - The data store contents will be modified once and used many times.
+// dynamic - The data store contents will be modified repeatedly and used many times.
+// draw - The data store contents are modified by the application,
 //   and used as the source for GL drawing and image specification commands.
-// READ - The data store contents are modified by reading data from the GL,
+// read - The data store contents are modified by reading data from the GL,
 //   and used to return that data when queried by the application.
-// COPY - The data store contents are modified by reading data from the GL,
+// copy - The data store contents are modified by reading data from the GL,
 //   and used as the source for GL drawing and image specification commands.
-enum class BufferUsage : GLenum {
-    STREAM_DRAW = GL_STREAM_DRAW,
-    STREAM_READ = GL_STREAM_READ,
-    STREAM_COPY = GL_STREAM_COPY,
-    STATIC_DRAW = GL_STATIC_DRAW,
-    STATIC_READ = GL_STATIC_READ,
-    STATIC_COPY = GL_STATIC_COPY,
-    DYNAMIC_DRAW = GL_DYNAMIC_DRAW,
-    DYNAMIC_READ = GL_DYNAMIC_READ,
-    DYNAMIC_COPY = GL_DYNAMIC_COPY,
+enum class buffer_usage : GLenum {
+    stream_draw = GL_STREAM_DRAW,
+    stream_read = GL_STREAM_READ,
+    stream_copy = GL_STREAM_COPY,
+    static_draw = GL_STATIC_DRAW,
+    static_read = GL_STATIC_READ,
+    static_copy = GL_STATIC_COPY,
+    dynamic_draw = GL_DYNAMIC_DRAW,
+    dynamic_read = GL_DYNAMIC_READ,
+    dynamic_copy = GL_DYNAMIC_COPY,
 };
 
-enum class BufferAccess : GLenum {
-    READ_ONLY = GL_READ_ONLY,
-    WRITE_ONLY = GL_WRITE_ONLY,
-    READ_WRITE = GL_READ_WRITE,
+enum class buffer_access : GLenum {
+    read_only = GL_READ_ONLY,
+    write_only = GL_WRITE_ONLY,
+    read_write = GL_READ_WRITE,
 };
 
-// TODO(@usatiynyan): solve unbind?
-template <typename DataType, BufferType type_, BufferUsage usage_>
-class Buffer : public meta::finalizer<Buffer<DataType, type_, usage_>> {
-public:
-    template <BufferAccess access_, std::size_t size_>
-    class Map : public meta::finalizer<Map<access_, size_>> {
-        friend class Buffer::Bind;
+template <typename T, buffer_type type, buffer_usage usage>
+class bound_buffer;
 
-        Map()
-            : meta::finalizer<Map<access_, size_>>{ [](Map&) { glUnmapBuffer(static_cast<GLenum>(type_)); } }, //
-              data_{ static_cast<DataType*>(
-                         ASSERT_VAL(glMapBuffer(static_cast<GLenum>(type_), static_cast<GLenum>(access_)))
-                     ),
-                     size_ } {}
+template <typename T, buffer_type type, buffer_access access, std::size_t size>
+class mapped_buffer;
 
-    public:
-        [[nodiscard]] auto data() const { return data_; }
-
-    private:
-        std::span<DataType, size_> data_;
-    };
-
-    class Bind {
-        friend class Buffer;
-
-        explicit Bind(Buffer& buffer) : buffer_{ buffer } {
-            LOG_DEBUG("glBindBuffer: {}", *buffer_);
-            glBindBuffer(static_cast<GLenum>(type_), *buffer_);
-        }
-
-    public:
-        template <std::size_t extent>
-        void set_data(std::span<const DataType, extent> data) {
-            LOG_DEBUG("glBufferData: size={}", data.size());
-            glBufferData(
-                static_cast<GLenum>(type_), sizeof(DataType) * data.size(), data.data(), static_cast<GLenum>(usage_)
-            );
-            buffer_.data_size_ = data.size();
-        }
-
-        template <std::size_t size_>
-        void initialize_data() {
-            LOG_DEBUG("glBufferData: size={}, data=nullptr", size_);
-            glBufferData(static_cast<GLenum>(type_), sizeof(DataType) * size_, nullptr, static_cast<GLenum>(usage_));
-            buffer_.data_size_ = size_;
-        }
-
-        template <GLuint index_>
-            requires(type_ == BufferType::SHADER_STORAGE)
-        void base() {
-            LOG_DEBUG("glBindBufferBase: {} index={}", *buffer_, index_);
-            glBindBufferBase(static_cast<GLenum>(type_), index_, *buffer_);
-        }
-
-        template <BufferAccess access_, std::size_t size_>
-        [[nodiscard]] auto map() {
-            return Map<access_, size_>{};
-        }
-
-        template <std::size_t size_>
-        [[nodiscard]] auto map() const {
-            return Map<BufferAccess::READ_ONLY, size_>{};
-        }
-
-    private:
-        Buffer& buffer_;
-    };
+template <typename T, buffer_type type, buffer_usage usage>
+class buffer : public meta::finalizer<buffer<T, type, usage>> {
+    friend class bound_buffer<T, type, usage>; // data_size_
 
 public:
-    Buffer()
-        : meta::finalizer<Buffer>{ [](Buffer& self) {
-              // TODO(@usatiynyan): more than one buffer?
+    buffer()
+        : meta::finalizer<buffer>{ [](buffer& self) {
               glDeleteBuffers(1, &self.object_);
               LOG_DEBUG("glDeleteBuffers: {}", self.object_);
           } },
-          object_{ [] {
+          internal_{ [] {
               GLuint buffer = 0;
+              // TODO(@usatiynyan): more than one buffer?
               glGenBuffers(1, &buffer);
               LOG_DEBUG("glGenBuffers: {}", buffer);
               return buffer;
           }() } {}
 
-    [[nodiscard]] GLuint operator*() const { return object_; }
-    [[nodiscard]] const Bind bind() const { return Bind{ *this }; }
-    [[nodiscard]] Bind bind() { return Bind{ *this }; }
+    [[nodiscard]] bound_buffer<T, type, usage> bind();
+    [[nodiscard]] const bound_buffer<T, type, usage> bind() const;
+
     [[nodiscard]] std::size_t data_size() const { return data_size_; }
+    [[nodiscard]] GLuint internal() const { return internal_; }
 
 private:
-    GLuint object_;
+    GLuint internal_;
     std::size_t data_size_ = 0;
 };
+
+template <typename T, buffer_type type, buffer_usage usage>
+class bound_buffer {
+public:
+    explicit bound_buffer(buffer<T, type, usage>& buffer) : buffer_{ buffer } {
+        LOG_DEBUG("glBindBuffer: {}", *buffer_);
+        glBindBuffer(static_cast<GLenum>(type), *buffer_);
+    }
+
+    template <std::size_t size>
+    void initialize_data() {
+        LOG_DEBUG("glBufferData: size={}, data=nullptr", size);
+        glBufferData(static_cast<GLenum>(type), sizeof(T) * size, nullptr, static_cast<GLenum>(usage));
+        buffer_.data_size_ = size;
+    }
+    template <std::size_t extent>
+    void set_data(std::span<const T, extent> data) {
+        LOG_DEBUG("glBufferData: size={}", data.size());
+        glBufferData(static_cast<GLenum>(type), sizeof(T) * data.size(), data.data(), static_cast<GLenum>(usage));
+        buffer_.data_size_ = data.size();
+    }
+
+    template <GLuint index>
+        requires(type == buffer_type::shader_storage)
+    void bind_base() {
+        LOG_DEBUG("glBindBufferBase: {} index={}", *buffer_, index);
+        glBindBufferBase(static_cast<GLenum>(type), index, *buffer_);
+    }
+
+    template <buffer_access access, std::size_t size>
+    [[nodiscard]] tl::optional<mapped_buffer<T, type, access, size>> map();
+    template <std::size_t size>
+    [[nodiscard]] tl::optional<mapped_buffer<T, type, buffer_access::read_only, size>> map() const;
+
+private:
+    buffer<T, type, usage>& buffer_;
+};
+
+template <typename T, buffer_type type, buffer_access access, std::size_t size>
+class mapped_buffer : public meta::finalizer<mapped_buffer<T, type, access, size>> {
+    mapped_buffer(T* data)
+        : meta::finalizer<mapped_buffer<T, type, access, size>>{ [](mapped_buffer&) {
+              glUnmapBuffer(static_cast<GLenum>(type));
+          } },
+          data_{ data, size } {}
+
+public:
+    static tl::optional<mapped_buffer> create() {
+        void* data = glMapBuffer(static_cast<GLenum>(type), static_cast<GLenum>(access));
+        if (data == nullptr) {
+            return tl::nullopt;
+        }
+        return mapped_buffer(static_cast<T*>(data));
+    }
+
+    [[nodiscard]] auto data() { return data_; }
+    [[nodiscard]] auto data() const { return data_; }
+
+private:
+    std::span<T, size> data_;
+};
+
+template <typename T, buffer_type type, buffer_usage usage>
+bound_buffer<T, type, usage> buffer<T, type, usage>::bind() {
+    return bound_buffer<T, type, usage>{ *this };
+}
+
+template <typename T, buffer_type type, buffer_usage usage>
+const bound_buffer<T, type, usage> buffer<T, type, usage>::bind() const {
+    return bound_buffer<T, type, usage>{ *this };
+}
+
+template <typename T, buffer_type type, buffer_usage usage>
+template <buffer_access access, std::size_t size>
+tl::optional<mapped_buffer<T, type, access, size>> bound_buffer<T, type, usage>::map() {
+    return mapped_buffer<T, type, access, size>::create();
+}
+
+template <typename T, buffer_type type, buffer_usage usage>
+template <std::size_t size>
+tl::optional<mapped_buffer<T, type, buffer_access::read_only, size>> bound_buffer<T, type, usage>::map() const {
+    return mapped_buffer<T, type, buffer_access::read_only, size>::create();
+}
+
 } // namespace sl::gfx
