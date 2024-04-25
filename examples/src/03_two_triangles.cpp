69f5c1cf1bf5dc2059040b44146d48503f1c1c47
UsatiyNyan <us4tiyny4n@gmail.com>
2024-04-26 01:56:43 +0200

    examples 03 restore

--- examples/src/03_two_triangles.cpp
+++ examples/src/03_two_triangles.cpp
@@ -1,13 +1,60 @@
 //
-// Created by usatiynyan on 11/28/23.
-//
 
 #include "sl/gfx.hpp"
+
+#include <sl/rt.hpp>
 
 #include <libassert/assert.hpp>
 #include <spdlog/spdlog.h>
 
-using namespace sl::gfx;
+namespace gfx = sl::gfx;
+namespace rt = sl::rt;
+
+auto create_triangle_shader(const std::filesystem::path& root, glm::fvec4 color) {
+    const auto fragment_shader_source = fmt::format(
+        "#version 460 core\n"
+        "out vec4 FragColor;\n"
+        "void main() {{\n"
+        "    FragColor = vec4({});"
+        "\n}}",
+        fmt::join(std::array{ color.r, color.g, color.b, color.a }, ", ")
+    );
+    const std::array<gfx::shader, 2> shaders{
+        *ASSERT_VAL(gfx::shader::load_from_file(gfx::shader_type::vertex, root / "shaders/03_two_triangles.vert")),
+        *ASSERT_VAL(gfx::shader::load_from_source(gfx::shader_type::fragment, fragment_shader_source)),
+    };
+    return *ASSERT_VAL(gfx::shader_program::build(std::span{ shaders }));
+};
+
+template <typename T>
+using element_buffer = gfx::buffer<T, gfx::buffer_type::element_array, gfx::buffer_usage::static_draw>;
+
+using buffers_type = std::tuple<
+    gfx::buffer<float, gfx::buffer_type::array, gfx::buffer_usage::static_draw>,
+    element_buffer<unsigned>,
+    gfx::vertex_array>;
+
+
+auto create_triangle_buffers(std::span<const float, 3 * 3> vertices) -> buffers_type {
+    gfx::vertex_array_builder va_builder;
+    va_builder.attribute<3, float>();
+    auto vb = va_builder.buffer<gfx::buffer_type::array, gfx::buffer_usage::static_draw>(vertices);
+    constexpr std::array<unsigned, 3> indices{ 0u, 1u, 2u };
+    auto eb = va_builder.buffer<gfx::buffer_type::element_array, gfx::buffer_usage::static_draw>(std::span{ indices });
+    auto va = std::move(va_builder).submit();
+    return std::make_tuple(std::move(vb), std::move(eb), std::move(va));
+};
+
+constexpr std::array left_triangle_vertices{
+    -0.0f, -0.5f, 0.0f, // bottom right
+    -1.0f, -0.5f, 0.0f, // bottom left
+    -1.0f, +0.5f, 0.0f, // top left
+};
+constexpr std::array right_triangle_vertices{
+    +1.0f, -0.5f, 0.0f, // bottom right
+    +0.0f, -0.5f, 0.0f, // bottom left
+    +0.0f, +0.5f, 0.0f, // top left
+};
 
 void debug_unbind() {
     glUseProgram(0);
@@ -15,76 +62,38 @@ void debug_unbind() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
-template <typename DataType>
-void draw(
-    const ShaderProgram& sp,
-    const VertexArray& va,
-    const Buffer<DataType, BufferType::ELEMENT_ARRAY, BufferUsage::STATIC_DRAW>& eb
-) {
+template <typename T>
+void draw(const gfx::shader_program& sp, const gfx::vertex_array& va, const element_buffer<T>& eb) {
     [[maybe_unused]] const auto sp_bind = sp.bind();
     const auto va_bind = va.bind();
-    glDrawElements(GL_TRIANGLES, eb.data_size(), type_map<DataType>::value, nullptr);
+    glDrawElements(GL_TRIANGLES, eb.data_size(), gfx::gl_type_query<T>, nullptr);
 }
 
-int main() {
+
+int main(int argc, char** argv) {
+    const rt::context rt_ctx{ argc, argv };
+    const auto root = rt_ctx.path().parent_path();
+
     spdlog::set_level(spdlog::level::debug);
 
-    auto ctx = *ASSERT_VAL(Context::create(Context::Options{ 4, 6, GLFW_OPENGL_CORE_PROFILE }));
-    const Size2I window_size{ 800, 600 };
-    const auto window = ASSERT_VAL(Window::create(ctx, "03_two_triangles", window_size));
-    (void)window->FramebufferSize_cb.connect([&window](GLsizei width, GLsizei height) {
-        Window::Current{ *window }.viewport(Vec2I{}, Size2I{ width, height });
+    auto ctx = ASSERT_VAL(gfx::context::create(gfx::context::options{ 4, 6, GLFW_OPENGL_CORE_PROFILE }));
+    constexpr glm::ivec2 window_size{ 800, 600 };
+    const auto window = ASSERT_VAL(gfx::window::create(*ctx, "03_two_triangles", window_size));
+
+    (void)window->frame_buffer_size_cb.connect([&window](glm::ivec2 size) {
+        gfx::current_window{ *window }.viewport(glm::ivec2{}, size);
     });
-    auto current_window = window->make_current(Vec2I{}, window_size, Color4F{ 0.2f, 0.3f, 0.3f, 1.0f });
 
-    constexpr auto create_triangle_shader = [](Color4F color4f) {
-        const auto fragment_shader_source = fmt::format(
-            "#version 460 core\n"
-            "out vec4 FragColor;\n"
-            "void main() {{\n"
-            "    FragColor = vec4({});"
-            "\n}}",
-            fmt::join(std::array{ color4f.red, color4f.green, color4f.blue, color4f.alpha }, ", ")
-        );
-        std::array<const Shader, 2> shaders{
-            *ASSERT_VAL(Shader::load_from_file(ShaderType::VERTEX, "shaders/03_two_triangles.vert")),
-            *ASSERT_VAL(Shader::load_from_source(ShaderType::FRAGMENT, fragment_shader_source)),
-        };
-        return *ASSERT_VAL(ShaderProgram::build(std::span{ shaders }));
-    };
+    constexpr glm::fvec4 clear_color{ 0.2f, 0.3f, 0.3f, 1.0f };
+    auto current_window = window->make_current(*ctx, glm::ivec2{}, window_size, clear_color);
 
-    using buffers_type = std::tuple<
-        Buffer<float, BufferType::ARRAY, BufferUsage::STATIC_DRAW>,
-        Buffer<unsigned, BufferType::ELEMENT_ARRAY, BufferUsage::STATIC_DRAW>,
-        VertexArray>;
-
-    constexpr auto create_triangle_buffers = [](std::span<const float, 3 * 3> vertices) -> buffers_type {
-        VertexArrayBuilder va_builder;
-        va_builder.attribute<3, float>();
-        auto vb = va_builder.buffer<BufferType::ARRAY, BufferUsage::STATIC_DRAW>(vertices);
-        constexpr std::array<unsigned, 3> indices{ 0u, 1u, 2u };
-        auto eb = va_builder.buffer<BufferType::ELEMENT_ARRAY, BufferUsage::STATIC_DRAW>(std::span{ indices });
-        auto va = std::move(va_builder).submit();
-        return std::make_tuple(std::move(vb), std::move(eb), std::move(va));
-    };
-
-    constexpr std::array left_triangle_vertices{
-        -0.0f, -0.5f, 0.0f, // bottom right
-        -1.0f, -0.5f, 0.0f, // bottom left
-        -1.0f, +0.5f, 0.0f, // top left
-    };
-    constexpr std::array right_triangle_vertices{
-        +1.0f, -0.5f, 0.0f, // bottom right
-        +0.0f, -0.5f, 0.0f, // bottom left
-        +0.0f, +0.5f, 0.0f, // top left
-    };
     const std::array triangles{
         std::pair{
-            create_triangle_shader(Color4F{ 1.0f, 0.5f, 0.2f, 1.0f }),
+            create_triangle_shader(root, glm::fvec4{ 1.0f, 0.5f, 0.2f, 1.0f }),
             create_triangle_buffers(std::span{ left_triangle_vertices }),
         },
         std::pair{
-            create_triangle_shader(Color4F{ 0.7f, 0.1f, 0.7f, 1.0f }),
+            create_triangle_shader(root, glm::fvec4{ 0.7f, 0.1f, 0.7f, 1.0f }),
             create_triangle_buffers(std::span{ right_triangle_vertices }),
         },
     };
@@ -94,19 +103,20 @@ int main() {
     // ^^^ debug
 
     while (!current_window.should_close()) {
+        ctx->poll_events();
         if (current_window.is_key_pressed(GLFW_KEY_ESCAPE)) {
             current_window.set_should_close(true);
         }
+
         current_window.clear(GL_COLOR_BUFFER_BIT);
 
-        for (const std::pair<ShaderProgram, buffers_type>& triangle : triangles) {
+        for (const std::pair<gfx::shader_program, buffers_type>& triangle : triangles) {
             const auto& [sp, buffers] = triangle;
             const auto& [vb, eb, va] = buffers;
             draw(sp, va, eb);
         }
 
         current_window.swap_buffers();
-        ctx.poll_events();
     }
 
     return 0;
