ef64901c192cba8b963291fad13376eedf070ed2
UsatiyNyan <us4tiyny4n@gmail.com>
2024-05-02 20:52:55 +0200

    examples 08, 09 restore

--- examples/src/08_imgui.cpp
+++ examples/src/08_imgui.cpp
@@ -7,27 +7,34 @@
 #include <libassert/assert.hpp>
 #include <spdlog/spdlog.h>
 
+namespace gfx = sl::gfx;
+
 int main() {
     spdlog::set_level(spdlog::level::debug);
 
-    const sl::gfx::Context::Options ctx_options{ 4, 6, GLFW_OPENGL_CORE_PROFILE };
-    auto ctx = *ASSERT_VAL(sl::gfx::Context::create(ctx_options));
-    const sl::gfx::Size2I window_size{ 800, 600 };
-    const auto window = ASSERT_VAL(sl::gfx::Window::create(ctx, "08_imgui", window_size));
-    (void)window->FramebufferSize_cb.connect([&window](GLsizei width, GLsizei height) {
-        sl::gfx::Window::Current{ *window }.viewport(sl::gfx::Vec2I{}, sl::gfx::Size2I{ width, height });
-    });
-    auto current_window =
-        window->make_current(sl::gfx::Vec2I{}, window_size, sl::gfx::Color4F{ 0.2f, 0.3f, 0.3f, 1.0f });
+    constexpr gfx::context::options ctx_options{ 4, 6, GLFW_OPENGL_CORE_PROFILE };
+    auto ctx = ASSERT_VAL(gfx::context::create(ctx_options));
+    constexpr glm::ivec2 window_size{ 800, 600 };
+    const auto window = ASSERT_VAL(gfx::window::create(*ctx, "08_imgui", window_size));
 
-    sl::gfx::ImGuiContext imgui_context{ ctx_options, *window };
+    (void)window->frame_buffer_size_cb.connect([&window](glm::ivec2 size) {
+        gfx::current_window{ *window }.viewport(glm::ivec2{}, size);
+    });
+
+    constexpr glm::fvec4 clear_color{ 0.2f, 0.3f, 0.3f, 1.0f };
+    auto current_window = window->make_current(*ctx, glm::ivec2{}, window_size, clear_color);
+
+    gfx::imgui_context imgui_context{ ctx_options, *window };
 
     [[maybe_unused]] ImGuiIO& io = ImGui::GetIO(); // Get the ImGui IO instance
 
     while (!current_window.should_close()) {
+        ctx->poll_events();
+
         if (current_window.is_key_pressed(GLFW_KEY_ESCAPE)) {
             current_window.set_should_close(true);
         }
+
         current_window.clear(GL_COLOR_BUFFER_BIT);
         imgui_context.new_frame();
 
@@ -35,7 +42,6 @@ int main() {
 
         imgui_context.render();
         current_window.swap_buffers();
-        ctx.poll_events();
     }
 
     return 0;
--- examples/src/09_implot.cpp
+++ examples/src/09_implot.cpp
@@ -7,26 +7,33 @@
 #include <libassert/assert.hpp>
 #include <spdlog/spdlog.h>
 
+namespace gfx = sl::gfx;
+
 int main() {
     spdlog::set_level(spdlog::level::debug);
 
-    const sl::gfx::Context::Options ctx_options{ 4, 6, GLFW_OPENGL_CORE_PROFILE };
-    auto ctx = *ASSERT_VAL(sl::gfx::Context::create(ctx_options));
-    const sl::gfx::Size2I window_size{ 800, 600 };
-    const auto window = ASSERT_VAL(sl::gfx::Window::create(ctx, "09_implot", window_size));
-    (void)window->FramebufferSize_cb.connect([&window](GLsizei width, GLsizei height) {
-        sl::gfx::Window::Current{ *window }.viewport(sl::gfx::Vec2I{}, sl::gfx::Size2I{ width, height });
-    });
-    auto current_window =
-        window->make_current(sl::gfx::Vec2I{}, window_size, sl::gfx::Color4F{ 0.2f, 0.3f, 0.3f, 1.0f });
+    constexpr gfx::context::options ctx_options{ 4, 6, GLFW_OPENGL_CORE_PROFILE };
+    auto ctx = ASSERT_VAL(gfx::context::create(ctx_options));
+    constexpr glm::ivec2 window_size{ 800, 600 };
+    const auto window = ASSERT_VAL(gfx::window::create(*ctx, "09_implot", window_size));
 
-    sl::gfx::ImGuiContext imgui_context{ ctx_options, *window };
-    sl::gfx::ImPlotContext implot_context{ imgui_context };
+    (void)window->frame_buffer_size_cb.connect([&window](glm::ivec2 size) {
+        gfx::current_window{ *window }.viewport(glm::ivec2{}, size);
+    });
+
+    constexpr glm::fvec4 clear_color{ 0.2f, 0.3f, 0.3f, 1.0f };
+    auto current_window = window->make_current(*ctx, glm::ivec2{}, window_size, clear_color);
+
+    gfx::imgui_context imgui_context{ ctx_options, *window };
+    gfx::implot_context implot_context{ imgui_context };
 
     while (!current_window.should_close()) {
+        ctx->poll_events();
+
         if (current_window.is_key_pressed(GLFW_KEY_ESCAPE)) {
             current_window.set_should_close(true);
         }
+
         current_window.clear(GL_COLOR_BUFFER_BIT);
         imgui_context.new_frame();
 
@@ -34,7 +41,6 @@ int main() {
 
         imgui_context.render();
         current_window.swap_buffers();
-        ctx.poll_events();
     }
 
     return 0;
