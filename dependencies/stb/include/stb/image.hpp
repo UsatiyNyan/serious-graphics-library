60d98802c2026adcaf52f74a9a90f1eaf6766dd7
UsatiyNyan <us4tiyny4n@gmail.com>
2025-02-17 02:21:12 +0100

    dependencies: stb: filepath crosplatform fix

--- dependencies/stb/include/stb/image.hpp
+++ dependencies/stb/include/stb/image.hpp
@@ -1,21 +1,30 @@
 //
-// Created by usatiynyan on 12/2/23.
+// Created by usatiynyan.
 //
 
 #pragma once
 
+#include <array>
 #include <filesystem>
 #include <memory>
+#include <span>
+
 #include <tl/optional.hpp>
 
 namespace stb {
-struct Image {
-    using Deleter = void (*)(uint8_t *);
 
-    std::unique_ptr<uint8_t, Deleter> data;
+struct Image {
+    using deleter_type = void (*)(uint8_t*);
+
+    std::unique_ptr<uint8_t, deleter_type> data;
     std::array<int, 2> dimensions;
     int channels;
 };
 
-tl::optional<Image> image_load(const std::filesystem::path& path, int desired_channels = 0, bool flip_vertically = true);
+tl::optional<Image>
+    image_load(const std::filesystem::path& path, int desired_channels = 0, bool flip_vertically = true);
+
+tl::optional<Image>
+    image_load_from_memory(std::span<const std::uint8_t> buffer, int desired_channels = 0, bool flip_vertically = true);
+
 } // namespace stb
--- dependencies/stb/src/image.cpp
+++ dependencies/stb/src/image.cpp
@@ -1,9 +1,11 @@
 //
-// Created by usatiynyan on 12/2/23.
+// Created by usatiynyan.
 //
 
 #include "stb/image.hpp"
 
+#include <bit>
+#include <fstream>
 #include <libassert/assert.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -15,14 +17,36 @@
 #undef STB_IMAGE_IMPLEMENTATION
 
 namespace stb {
+
 tl::optional<Image> image_load(const std::filesystem::path& path, int desired_channels, bool flip_vertically) {
+    std::ifstream file{ path, std::ios::binary | std::ios::ate };
+    if (!file) {
+        return tl::nullopt;
+    }
+
+    const std::streamsize size = file.tellg();
+    if (size < 0) {
+        return tl::nullopt;
+    }
+    file.seekg(0, std::ios::beg);
+
+    std::vector<std::uint8_t> buffer(size);
+    if (!file.read(std::bit_cast<char*>(buffer.data()), size)) {
+        return tl::nullopt;
+    }
+
+    return image_load_from_memory(std::span(std::as_const(buffer)), desired_channels, flip_vertically);
+}
+
+tl::optional<Image>
+    image_load_from_memory(std::span<const std::uint8_t> buffer, int desired_channels, bool flip_vertically) {
     stbi_set_flip_vertically_on_load(flip_vertically);
 
     int width = 0;
     int height = 0;
     int channels = 0;
-    std::unique_ptr<uint8_t, Image::Deleter> data{
-        stbi_load(path.c_str(), &width, &height, &channels, desired_channels),
+    std::unique_ptr<uint8_t, Image::deleter_type> data{
+        stbi_load_from_memory(buffer.data(), buffer.size_bytes(), &width, &height, &channels, desired_channels),
         [](uint8_t* image_data) { stbi_image_free(static_cast<void*>(image_data)); },
     };
     if (!data) {
@@ -30,4 +54,5 @@ tl::optional<Image> image_load(const std::filesystem::path& path, int desired_ch
     }
     return Image{ std::move(data), { width, height }, channels };
 }
+
 } // namespace stb
