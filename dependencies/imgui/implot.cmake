671244a1bd5bc3eed4ec49efb112680eaa483dd0
UsatiyNyan <us4tiyny4n@gmail.com>
2025-02-21 23:28:48 +0100

    implot demo in separate library

--- dependencies/imgui/implot.cmake
+++ dependencies/imgui/implot.cmake
@@ -4,17 +4,24 @@ cpmaddpackage(
         GIT_REPOSITORY "git@github.com:epezent/implot.git"
         GIT_TAG v0.16
 )
+
 add_library(implot STATIC
         "${implot_SOURCE_DIR}/implot.cpp"
         "${implot_SOURCE_DIR}/implot_items.cpp"
 
-        "${implot_SOURCE_DIR}/implot_demo.cpp"
 )
 target_include_directories(implot
         PUBLIC "${implot_SOURCE_DIR}"
 )
-
 sl_target_link_system_libraries(implot
         PUBLIC
         imgui
+)
+
+add_library(implot_demo STATIC
+        "${implot_SOURCE_DIR}/implot_demo.cpp"
+)
+sl_target_link_system_libraries(implot_demo
+        PUBLIC
+        implot
 )
